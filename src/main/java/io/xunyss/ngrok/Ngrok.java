666795cd33bb85c188b447d001ae827fe5ccafbf
xunyss@gmail.com <xunyss@gmail.com>
2018-03-03 18:18:19 +0900

    add watchdog to common.exec

--- src/main/java/io/xunyss/ngrok/Ngrok.java
+++ src/main/java/io/xunyss/ngrok/Ngrok.java
@@ -1,8 +1,15 @@
 package io.xunyss.ngrok;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import io.xunyss.commons.exec.ExecuteException;
 import io.xunyss.commons.exec.ProcessExecutor;
-import io.xunyss.commons.exec.PumpStreamHandler;
+import io.xunyss.commons.exec.ResultHandler;
+import io.xunyss.commons.exec.StreamHandler;
+import io.xunyss.commons.exec.WatchDog;
 import io.xunyss.commons.lang.ArrayUtils;
 
 /**
@@ -20,7 +27,9 @@ public class Ngrok {
 	// http://www.ultrahook.com/
 	//----------------------------------------------------------------------------------------------
 	
-	private Config config;
+	private final Config config;
+	
+	NgrokWatchDog watchDog = new NgrokWatchDog();
 	
 	
 	public Ngrok(Config config) {
@@ -32,11 +41,23 @@ public class Ngrok {
 				BinaryManager.getInstance().getExecutable(), "start",
 				"-config", config.getPath()
 		};
-
+		
+		
 		ProcessExecutor processExecutor = new ProcessExecutor();
-		processExecutor.setStreamHandler(new PumpStreamHandler());
+		processExecutor.setStreamHandler(new NgrokProcessStreamHandler());
+		processExecutor.setWatchDog(watchDog);
+		
 		try {
-			processExecutor.execute(ArrayUtils.add(commands, tunnelNames));
+			processExecutor.execute(ArrayUtils.add(commands, tunnelNames), new ResultHandler() {
+				@Override
+				public void onProcessComplete(int exitValue) {
+					System.err.println("onProcessCompleetet : " + exitValue);
+				}
+				@Override
+				public void onProcessFailed(ExecuteException ex) {
+					//
+				}
+			});
 		}
 		catch (ExecuteException ex) {
 			ex.printStackTrace();
@@ -44,6 +65,54 @@ public class Ngrok {
 	}
 	
 	public void kill() {
+		watchDog.destroyNgrok();
+	}
 	
+	
+	private class NgrokProcessStreamHandler extends StreamHandler {
+		
+		@Override
+		public void start() {
+			InputStream inputStream = getProcessInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			
+			int c = 0;
+			String line;
+			try {
+				while ((line = reader.readLine()) != null) {
+					c++;
+					System.out.println(line);
+					
+					if (c > 40) {
+						// exit
+						System.err.println("이제 그만 프로세스를 종료할까 합니다");
+						watchDog.destroyNgrok();
+					}
+				}
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void stop() {
+			System.err.println("Ngrok 스트림 핸들러 중지됨");
+		}
+	}
+	
+	private class NgrokWatchDog extends WatchDog {
+		
+		@Override
+		protected void start() {
+		}
+		
+		@Override
+		protected void stop() {
+		}
+		
+		public void destroyNgrok() {
+			destroyProcess();
+		}
 	}
 }
--- src/test/java/io/xunyss/ngrok/NgrokTest.java
+++ src/test/java/io/xunyss/ngrok/NgrokTest.java
@@ -1,30 +1,30 @@
 package io.xunyss.ngrok;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
-import io.xunyss.commons.exec.ExecuteException;
+import java.util.Date;
 
 public class NgrokTest {
 	
-	@Ignore
-	@Test
-	public void test() throws Exception{
+	public static void main(String[] args) throws Exception {
 	
 		Config config = ConfigBuilder.create()
 				.setAuthtoken(null)
 				.setLogLevel("debug")
 				.setLogFormat("logfmt")
 				.setLog("stdout")
-				.addTunnel(ConfigBuilder.createTunnel("httpbin")
+				.addTunnel(ConfigBuilder.createTunnel("xtunnel")
 						.setProto("http")
 						.setAddr("9797")
 				)
 				.build();
 		
 		Ngrok ngrok = new Ngrok(config);
-//		ngrok.run("httpbin");
+		ngrok.run("xtunnel1");
 		
+		System.err.println(new Date());
 		Thread.sleep(5000);
+		System.err.println("5초 잠");
+		System.err.println(new Date());
+		
+		ngrok.kill();
 	}
 }
