f2f6c5667e1ce336e664dc1428ede04deaf2bdb2
xunyss@gmail.com <xunyss@gmail.com>
2018-03-11 23:39:13 +0900

    test delete executable file

---
+++ src/test/java/io/xunyss/ngrok/BinaryManagerTest.java
@@ -0,0 +1,41 @@
+package io.xunyss.ngrok;
+
+import java.io.File;
+
+import io.xunyss.commons.io.FileUtils;
+
+/**
+ *
+ * @author XUNYSS
+ */
+public class BinaryManagerTest {
+	
+	public void deleteProcessFile() throws Exception {
+		final String source = "D:/downloads/ngrok.exe";
+		final String exe = "ngrok.exe";
+		
+		FileUtils.copy(new File(source), new File(FileUtils.getTempDirectory(), exe));
+		
+		String executable = FileUtils.getTempDirectory().getAbsolutePath() + FileUtils.FILE_SEPARATOR_CHAR + exe;
+		final Process process = Runtime.getRuntime().exec(executable);
+		
+		new Thread() {
+			@Override
+			public void run() {
+				process.destroy();
+				boolean success = new File("C:\\Users\\xunyss\\AppData\\Local\\Temp\\ngrok.exe").delete();
+				System.out.println("delete success: " + success);
+				/*
+				 * success 는 주로 true 이지만, 가끔씩 false 가 됨 ㅜㅠ
+				 * proc.destroy() 직후에는 executable file 이 지워지지 않을 수 있음
+				 */
+			}
+		}.start();
+		
+		process.waitFor();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new BinaryManagerTest().deleteProcessFile();
+	}
+}
